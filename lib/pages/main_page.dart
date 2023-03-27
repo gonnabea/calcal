c4759b030e4458e4ffd2d100ba056a84bd6274a9
gonnabea <winning1993@naver.com>
2023-03-27 23:04:24 +0900

    [ADD] 메인페이지 레이아웃 정리

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -16,75 +16,106 @@ class _MainPageState extends State<MainPage> {
         body: Column(
       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Row(
-          children: [
-            const FaIcon(FontAwesomeIcons.person, size: 80),
-            Column(
-              children: const [
-                Text("1,000 kcal",
-                    style: TextStyle(
-                        color: Colors.grey, fontWeight: FontWeight.w600)),
-                Text("1,000 kcal",
-                    style:
-                        TextStyle(fontSize: 24, fontWeight: FontWeight.w300)),
-              ],
+        Container(
+          padding: const EdgeInsets.only(top: 40),
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 80),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                children: [
+                  const FaIcon(FontAwesomeIcons.person, size: 80),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: const [
+                        Text("1,000 kcal",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600)),
+                        Text("1,000 kcal",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.w300)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-        SizedBox(
-          width: 300,
-          child: TextFormField(
-            autovalidateMode: AutovalidateMode.always,
-            onSaved: (value) {},
-            decoration: const InputDecoration(labelText: "음식 검색"),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return '음식 이름을 입력하세요!';
-              }
-
-              return null;
-            },
           ),
         ),
-        SingleChildScrollView(
-          child: Container(
+        Container(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: SizedBox(
+            width: 300,
+            child: TextFormField(
+              autovalidateMode: AutovalidateMode.always,
+              onSaved: (value) {},
+              decoration: const InputDecoration(labelText: "음식 검색"),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '음식 이름을 입력하세요!';
+                }
+
+                return null;
+              },
+            ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 80),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              OutlinedButton(onPressed: null, child: Text("전체")),
+              OutlinedButton(onPressed: null, child: Icon(Icons.add)),
+            ],
+          ),
+        ),
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 20),
               child: Column(children: const [
-            ListCard(
-              title: "떡볶이",
-              imgUrl: 'assets/sample.png',
-              kcalNum: 300,
+                ListCard(
+                  title: "떡볶이",
+                  imgUrl: 'assets/sample.png',
+                  kcalNum: 300,
+                ),
+                ListCard(
+                  title: "떡볶이",
+                  imgUrl: 'assets/sample.png',
+                  kcalNum: 300,
+                ),
+                ListCard(
+                  title: "떡볶이",
+                  imgUrl: 'assets/sample.png',
+                  kcalNum: 300,
+                ),
+                ListCard(
+                  title: "떡볶이",
+                  imgUrl: 'assets/sample.png',
+                  kcalNum: 300,
+                ),
+                ListCard(
+                  title: "떡볶이",
+                  imgUrl: 'assets/sample.png',
+                  kcalNum: 300,
+                ),
+                ListCard(
+                  title: "떡볶이",
+                  imgUrl: 'assets/sample.png',
+                  kcalNum: 300,
+                ),
+                ListCard(
+                  title: "떡볶이",
+                  imgUrl: 'assets/sample.png',
+                  kcalNum: 300,
+                ),
+              ]),
             ),
-            ListCard(
-              title: "떡볶이",
-              imgUrl: 'assets/sample.png',
-              kcalNum: 300,
-            ),
-            ListCard(
-              title: "떡볶이",
-              imgUrl: 'assets/sample.png',
-              kcalNum: 300,
-            ),
-            ListCard(
-              title: "떡볶이",
-              imgUrl: 'assets/sample.png',
-              kcalNum: 300,
-            ),
-            ListCard(
-              title: "떡볶이",
-              imgUrl: 'assets/sample.png',
-              kcalNum: 300,
-            ),
-            ListCard(
-              title: "떡볶이",
-              imgUrl: 'assets/sample.png',
-              kcalNum: 300,
-            ),
-            ListCard(
-              title: "떡볶이",
-              imgUrl: 'assets/sample.png',
-              kcalNum: 300,
-            ),
-          ])),
+          ),
         )
       ],
     ));
--- lib/widgets/list_card.dart
+++ lib/widgets/list_card.dart
@@ -17,13 +17,20 @@ class ListCard extends StatelessWidget {
       width: 300,
       height: 100,
       child: Row(children: [
-        Image.asset(imgUrl, width: 80, height: 80, fit: BoxFit.fill),
+        Image.asset(
+          imgUrl,
+          width: 80,
+          height: 80,
+        ),
         Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(title, style: const TextStyle(fontWeight: FontWeight.w600)),
             Text("$kcalNum kcal",
-                style: const TextStyle(fontWeight: FontWeight.w300))
+                style: const TextStyle(
+                  fontWeight: FontWeight.w300,
+                )),
           ],
         ),
       ]),
